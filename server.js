43843ee22c0181259085ac1aa996723dd9199c74
buddhi549 <humbleandwise16@gmail.com>
2022-04-30 16:29:29 +0530

    Initialized messagebird SDK

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const connectDB = require('./config/db')
 const errorHandler = require('./middleware/error')
 
+//load and initialize messagebird SDK
+var messagebird = require('messagebird')('Ho6tvwvQO5Kmp3OIOMg3ikmYl')
+
 connectDB();
 
 const app = express();
